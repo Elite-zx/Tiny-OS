e96ac109b855a064b178e1d69b4eb41dbe4fabb9
EliteZX <elite2022zx@gmail.com>
2023-11-07 18:48:39 +0800

    Correct the loader's kernel loading function ---> Modify kernel.bin from 64-bit to 32-bit so that the kernel segments can be copied to the correct location.

--- S5/c/kernel/gen.sh
+++ S5/c/kernel/gen.sh
@@ -1,4 +1,4 @@
-cd  "$(dirname "$0")"
-gcc -c -o main.o main.c && 
-ld main.o -Ttext 0xc0001500 -e main -o kernel.bin &&
+gcc -m32 -c -o main.o main.c &&
+ld -m elf_i386 main.o -Ttext 0xc0001500 -e main -o kernel.bin &&
 dd if=kernel.bin of=/home/elite-zx/bochs/hd60M.img bs=512 count=200 seek=9 conv=notrunc
+
