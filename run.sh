4419965fa59dfeda8267b06d7e5b1bb2cb06cac6
EliteZX <elite2022zx@gmail.com>
2023-11-07 17:03:06 +0800

    negligible modification

--- run.sh
+++ run.sh
@@ -1,7 +1,5 @@
-current_dir=$(pwd)
- 
-nasm -I ${current_dir}/include/ -o ${current_dir}/mbr.bin ${current_dir}/mbr.S
-nasm -I ${current_dir}/include/ -o ${current_dir}/loader.bin ${current_dir}/loader.S
-dd if=${current_dir}/mbr.bin of=/home/elite-zx/bochs/hd60M.img bs=512 count=1 conv=notrunc
-dd if=${current_dir}/loader.bin of=/home/elite-zx/bochs/hd60M.img bs=512 count=4 seek=2 conv=notrunc
+nasm -I include/ -o mbr.bin mbr.S
+nasm -I include/ -o loader.bin loader.S
+dd if=mbr.bin of=/home/elite-zx/bochs/hd60M.img bs=512 count=1 conv=notrunc
+dd if=loader.bin of=/home/elite-zx/bochs/hd60M.img bs=512 count=4 seek=2 conv=notrunc
 
