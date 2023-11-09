eee40f092bbae6461ba7f4504c6584e5ec353881
EliteZX <elite2022zx@gmail.com>
2023-11-09 21:27:32 +0800

    Add new functionality to kernel: print function to print a string

--- S6/kernel/main.c
+++ S6/kernel/main.c
@@ -5,17 +5,7 @@
 #include "print.h"
 
 int main() {
-  put_char('k');
-  put_char('e');
-  put_char('r');
-  put_char('n');
-  put_char('e');
-  put_char('l');
-  put_char('\n');
-  put_char('1');
-  put_char('1');
-  put_char('\b');
-  put_char('1');
-  put_char('.');
-  put_char('9');
+  put_str("I am kernel!\n");
+  while (1)
+    ;
 }
--- S6/lib/kernel/print.h
+++ S6/lib/kernel/print.h
@@ -6,4 +6,5 @@
 #define __LIB_KERNEL_PRINT_H
 #include "stdint.h"
 void put_char(uint8_t char_in_ascii);
+void put_str(char* message);
 #endif
