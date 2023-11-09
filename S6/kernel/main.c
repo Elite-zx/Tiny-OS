32071e40a38c2313b3a4771b53565dbdfd3d450e
EliteZX <elite2022zx@gmail.com>
2023-11-09 20:25:22 +0800

    Add kernel functionality: print function to print a single character

---
+++ S6/kernel/main.c
@@ -0,0 +1,21 @@
+//------------------------
+// Author: Xun Morris |
+// Time: 2023-11-09 |
+//------------------------
+#include "print.h"
+
+int main() {
+  put_char('k');
+  put_char('e');
+  put_char('r');
+  put_char('n');
+  put_char('e');
+  put_char('l');
+  put_char('\n');
+  put_char('1');
+  put_char('1');
+  put_char('\b');
+  put_char('1');
+  put_char('.');
+  put_char('9');
+}
---
+++ S6/lib/kernel/print.h
@@ -0,0 +1,9 @@
+//------------------------
+// Author: Xun Morris |
+// Time: 2023-11-09 |
+//------------------------
+#ifndef __LIB_KERNEL_PRINT_H
+#define __LIB_KERNEL_PRINT_H
+#include "stdint.h"
+void put_char(uint8_t char_in_ascii);
+#endif
