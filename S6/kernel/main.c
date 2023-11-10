b597a5116f28b69c09d48625a602ace251f7d4ca
EliteZX <elite2022zx@gmail.com>
2023-11-10 11:27:32 +0800

    Add new functionality to kernel: print function to print a integer

--- S6/kernel/main.c
+++ S6/kernel/main.c
@@ -6,6 +6,14 @@
 
 int main() {
   put_str("I am kernel!\n");
+  put_int(2);
+  put_char('\n');
+  put_int(0x0000);
+  put_char('\n');
+  put_int(0x0002);
+  put_char('\n');
+  put_int(0x0003);
+  put_char('\n');
   while (1)
     ;
 }
--- S6/lib/kernel/print.h
+++ S6/lib/kernel/print.h
@@ -7,4 +7,5 @@
 #include "stdint.h"
 void put_char(uint8_t char_in_ascii);
 void put_str(char* message);
+void put_int(uint32_t num);
 #endif
