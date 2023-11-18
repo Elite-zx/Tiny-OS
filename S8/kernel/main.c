4606673c522dd252b25fe77de3324b69a2b5b5b2
EliteZX <elite2022zx@gmail.com>
2023-11-18 21:39:59 +0800

    Add new functionality to kernel: memory management system

--- S8/kernel/main.c
+++ S8/kernel/main.c
@@ -5,11 +5,16 @@
 
 #include "debug.h"
 #include "init.h"
+#include "memory.h"
 #include "print.h"
 
 int main() {
   put_str("I am kernel\n");
   init_all();
+  void *addr = get_kernel_pages(3);
+  put_str("\n get_kernel_pages start vaddr is ");
+  put_int((uint32_t)addr);
+  put_str("\n");
   while (1)
     ;
   return 0;
